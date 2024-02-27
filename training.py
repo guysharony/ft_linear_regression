0f8669798661936e5b402aec0a03f4d89ef2ff90
Guy SHARONY <gsharony@bess-f1r3s9.clusters.42paris.fr>
2024-02-27 10:45:34 +0100

    Fixed

--- training.py
+++ training.py
@@ -1,49 +1,57 @@
+import sys
 import pickle
 import pandas as pd
 
 from src.graphics import plot_training
 from src.normalization import normalize_min_max
 from src.normalization import denormalize_min_max
-from src.linear_regression import LinearRegression
+from src.LinearRegression import LinearRegression
 
 
 def main():
-    dataset = pd.read_csv("./dataset/data.csv")
+    try:
+        if len(sys.argv) != 2:
+            print('Usage: python training.py <csv_file_path>')
+            sys.exit(1)
 
-    x = dataset["km"].values
-    y = dataset["price"].values
+        dataset = pd.read_csv(sys.argv[1])
 
-    # Mileage minimum and maximum
-    x_minimum, x_maximum = min(x), max(x)
+        x = dataset["km"].values
+        y = dataset["price"].values
 
-    # Prices minimum and maximum
-    y_minimum, y_maximum = min(y), max(y)
+        # Mileage minimum and maximum
+        x_minimum, x_maximum = min(x), max(x)
 
-    # Normalizing mileages
-    x_normalized = normalize_min_max(x, x_minimum, x_maximum)
-    y_normalized = normalize_min_max(y, y_minimum, y_maximum)
+        # Prices minimum and maximum
+        y_minimum, y_maximum = min(y), max(y)
 
-    # Initializing model
-    model = LinearRegression(
-        learning_rate=0.1,
-        max_iterations=1_000,
-    )
+        # Normalizing mileages
+        x_normalized = normalize_min_max(x, x_minimum, x_maximum)
+        y_normalized = normalize_min_max(y, y_minimum, y_maximum)
 
-    # Gradient descent
-    errors = model.gradient_descent(x_normalized, y_normalized)
-    print(f"MSE Evolution [{errors[0]}] -> [{errors[-1]}]")
+        # Initializing model
+        model = LinearRegression(
+            learning_rate=0.1,
+            max_iterations=1_000,
+        )
 
-    # Saving parameters
-    model.save_parameters(x, y)
+        # Gradient descent
+        errors = model.gradient_descent(x_normalized, y_normalized)
+        print(f"MSE Evolution [{errors[0]}] -> [{errors[-1]}]")
 
-    # Predictions
-    y_predictions_normalized = model.prediction(x_normalized)
+        # Saving parameters
+        model.save_parameters(x, y)
 
-    # De-normalizing predictions
-    y_predictions_denormalized = denormalize_min_max(y_predictions_normalized, y_minimum, y_maximum)
+        # Predictions
+        y_predictions_normalized = model.prediction(x_normalized)
 
-    # Plotting data
-    plot_training(x, y, y_predictions_denormalized, errors)
+        # De-normalizing predictions
+        y_predictions_denormalized = denormalize_min_max(y_predictions_normalized, y_minimum, y_maximum)
+
+        # Plotting data
+        plot_training(x, y, y_predictions_denormalized, errors)
+    except Exception as err:
+        print(f'Error: {err}')
 
 if __name__ == "__main__":
     main()
